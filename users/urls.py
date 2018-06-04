8f76c049c1d9cbc7d8b21738a314823afc3918c3
强子 <hezq168@gmail.com>
2018-06-04 11:34:11 +0800

    'init'

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/4 11:02
+# @Author  : Hezq
+# @Contact : Hezq168@gmail.com
+# @File    : urls.py
+# @Project : django_auth_example
+# @Software: PyCharm
+
+
+from django.urls import path, include
+from users import views
+
+urlpatterns = [
+
+]
